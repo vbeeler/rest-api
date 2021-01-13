44ed845246509e89979e5b3510897bb29bf688ae
Vanessa Beeler <vbeeler@calpoly.edu>
2021-01-13 11:46:53 -0800

    finished flask assignment

--- sample_backend.py
+++ sample_backend.py
@@ -7,61 +7,80 @@ app = Flask(__name__)
 def hello_world():
 	return 'Hello, world!'
 
-@app.route('/users', methods=['GET', 'POST'])
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def get_users():
-   if request.method == 'GET':
-      search_username = request.args.get('name')
-      if search_username :
-         subdict = {'users_list' : []}
-         for user in users['users_list']:
-            if user['name'] == search_username:
-               subdict['users_list'].append(user)
-         return subdict
-      return users
-   elif request.method == 'POST':
-      userToAdd = request.get_json()
-      users['users_list'].append(userToAdd)
-      resp = jsonify(success=True)
-      #resp.status_code = 200 #optionally, you can always set a response code. 
-      # 200 is the default code for a normal response
-      return resp
+	if request.method == 'GET':
+		search_username = request.args.get('name')
+		search_job =request.args.get('job')
+		if search_username and search_job :
+			subdict = {'users_list' : []}
+			for user in users['users_list']:
+				if user['name'] == search_username and user['job'] == search_job:
+					subdict['users_list'].append(user)
+			return subdict
+		elif search_username :
+			subdict = {'users_list' : []}
+			for user in users['users_list']:
+				if user['name'] == search_username:
+					subdict['users_list'].append(user)
+			return subdict
+		return users
+	elif request.method == 'POST':
+		userToAdd = request.get_json()
+		users['users_list'].append(userToAdd)
+		resp = jsonify(success=True)
+		#resp.status_code = 200 #optionally, you can always set a response code. 
+		# 200 is the default code for a normal response
+		return resp
+	elif request.method == 'DELETE':
+		search_id = request.args.get('id')
+		resp = jsonify(success=False)
+		if search_id :
+			subdict = []
+			for user in users['users_list']:
+				if user['id'] != search_id:
+					subdict.append(user)
+				else:
+					resp = jsonify(success=True)
+			users['users_list'] = subdict
+		return resp
 
 @app.route('/users/<id>')
 def get_user(id):
-   if id :
-      for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
-   return users
+	if id :
+		for user in users['users_list']:
+			if user['id'] == id:
+				return user
+		return ({})
+	return users
 
 users = { 
-   'users_list' :
-   [
-      { 
-         'id' : 'xyz789',
-         'name' : 'Charlie',
-         'job': 'Janitor',
-      },
-      {
-         'id' : 'abc123', 
-         'name': 'Mac',
-         'job': 'Bouncer',
-      },
-      {
-         'id' : 'ppp222', 
-         'name': 'Mac',
-         'job': 'Professor',
-      }, 
-      {
-         'id' : 'yat999', 
-         'name': 'Dee',
-         'job': 'Aspring actress',
-      },
-      {
-         'id' : 'zap555', 
-         'name': 'Dennis',
-         'job': 'Bartender',
-      }
-   ]
+	'users_list' :
+	[
+		{ 
+			'id' : 'xyz789',
+			'name' : 'Charlie',
+			'job': 'Janitor',
+		},
+		{
+			'id' : 'abc123', 
+			'name': 'Mac',
+			'job': 'Bouncer',
+		},
+		{
+			'id' : 'ppp222', 
+			'name': 'Mac',
+			'job': 'Professor',
+		}, 
+		{
+			'id' : 'yat999', 
+			'name': 'Dee',
+			'job': 'Aspring actress',
+		},
+		{
+			'id' : 'zap555', 
+			'name': 'Dennis',
+			'job': 'Bartender',
+		}
+	]
 }
